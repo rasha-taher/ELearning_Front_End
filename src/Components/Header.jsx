034dd55fc7fee681ee2130d49228939c766c0f9e
rasha <rashataher1245@gmail.com>
2023-10-13 11:13:13 +0300

    added folders

---
+++ src/Components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <div className="menu">
+            <div>
+                <p className='logo'>MindX</p>
+
+            </div>
+            <div>
+                
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Header
