dc102200d27317c6153014e3b4c859d34f5a835c
Shirineahmad <shirine.ahmad@hotmail.com>
2023-11-02 15:36:05 +0200

    done without quiz

--- src/pages/Learning.jsx
+++ src/pages/Learning.jsx
@@ -6,21 +6,26 @@ import ChapterInfo from "./ChapterInfo";
 import Test from "./Test";
 import axios from "axios";
 import { useState } from "react";
+
 const Learning = () => {
-  
-  const [data, setData] = useState(null)
-  const [chapter, setChapter] = useState(null)
+  const [data, setData] = useState(null);
+  const [chapter, setChapter] = useState([]);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const [firstResponse, secondResponse] = await Promise.all([
-          axios.get("http://localhost:8000/language/getLanguageById/11"),
-          axios.get("http://localhost:8000/chapter/getAllChapter"),
-        ]);
-        setData(firstResponse.data);
-        console.log(firstResponse.data);
-        setChapter(secondResponse.data);
-        console.log(secondResponse.data);
+        const res1 = await axios.get(
+          "http://localhost:8000/language/getLanguageById/11"
+        );
+        const res2 = await axios.get(
+          "http://localhost:8000/chapter/getAllChapter"
+        );
+
+        setData(res1.data.data);
+        setChapter(res2.data.data);
+
+        console.log(data, "data");
+        console.log(chapter, "chap");
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -28,21 +33,8 @@ const Learning = () => {
 
     fetchData();
   }, []);
+  
 
-  // const comp = chapter.data((info) => (
-  //   <Route
-  //     path="/chapterInfo"
-  //     element={<ChapterInfo />}
-  //     key={info.language_content_id}
-  //     title={info.chapter_title}
-  //     chapterContent={info.chapter_content}
-  //     question1={info.question}
-  //     question2={info.question2}
-  //     question3={info.question3}
-  //   />
-  // ));
-
- 
   return (
     <div className="sectionSideBar">
       <div className="learning-side">
@@ -52,18 +44,19 @@ const Learning = () => {
             width="50"
             height="40"
             fill="currentColor"
-            class="bi bi-arrow-left-circle-fill"
+            className="bi bi-arrow-left-circle-fill back-arow ms-3"
             viewBox="0 0 16 16"
-            className="back-arow ms-3"
           >
             <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z" />
           </svg>
 
-          {data && data.data[0] && data.data[0].language_name}
+          {data && data.data && data.data[0] && data.data[0].language_name}
         </div>
         <div className="text-center">
           <img
-            src={data && data.data[0] && data.data[0].language_picture}
+            src={
+              data && data.data && data.data[0] && data.data[0].language_picture
+            }
             className="learning-course-img"
             alt="no image"
           />
@@ -71,39 +64,58 @@ const Learning = () => {
         <div className="clearfix text-align-center">
           <div className="score-level"></div>
         </div>
-        <Link to="/learning/chapterInfo">
-          {chapter &&
-            chapter.data &&
-            chapter.data.map((info) => (
+
+        {chapter &&
+          chapter.map((info) => (
+            <Link
+              to={`/learning/chapterInfo/${info.language_content_id}/${info.chapter_title}`}
+              key={info.language_content_id}
+            >
               <Chapter
                 key={info.language_content_id}
                 title={info.chapter_title}
               />
-            ))}
-        </Link>
+            </Link>
+          ))}
+
         <Link to="/learning/Test">
           <Chapter />
         </Link>
       </div>
       <div className="contain">
-        if()
-        <ChapterInfo />
-        
-        {/* {chapter &&
-          chapter.data &&
-          chapter.data((info) => (
-            <ChapterInfo
-              key={info.language_content_id}
-              title={info.chapter_title}
-            />))} */}
-         
+        {chapter &&
+          console.log(
+            chapter.language_content_id,
+            chapter.language_content_id,
+            chapter.chapter_title,
+            chapter.language_content_id,
+            chapter.chapter_title,
+            chapter.chapter_content,
+            chapter.question,
+            chapter.question2,
+            chapter.question3
+          )}
+        <Routes>
+          {chapter &&
+            chapter.map((info) => (
+              <Route
+                key={info.language_content_id}
+                path={`/chapterInfo/${info.language_content_id}/${info.chapter_title}`}
+                element={
+                  <ChapterInfo
+                    key={info.language_content_id}
+                    title={info.chapter_title}
+                    chapterContent={info.chapter_content}
+                    question1={info.question}
+                    question2={info.question2}
+                    question3={info.question3}
+                  />
+                }
+              />
+            ))}
 
-        {/* <Routes>
-        
-      
-          <Route path="/chapterInfo" element={<ChapterInfo />} />
           <Route path="/Test" element={<Test />} />
-        </Routes> */}
+        </Routes>
       </div>
     </div>
   );
