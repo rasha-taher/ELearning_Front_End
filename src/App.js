d9a1eb48cf517d94530d2fcb6605a4823bbd07a1
Shirineahmad <shirine.ahmad@hotmail.com>
2023-11-05 11:42:31 +0200

    add categories

--- src/App.js
+++ src/App.js
@@ -8,22 +8,22 @@ import "./styles/responsive.css";
 import BecomeATeacher from "./Components/BecomeATeacher";
 import About from "./Components/about";
 import Contact from "./Components/Contact";
+import Categories from "./Components/Categories.jsx";
 
 function App() {
   return (
     <div className="App">
-    
-        <Routes>
-          <Route path="/" element={<HomePage />}></Route>
-          <Route path="ProfilePage" element={<ProfilePage />} />
-          <Route path="About" element={<About />} />
-          <Route path="Contact" element={<Contact />} />
-          <Route path="SignIn" element={<SignIn />} />
-          <Route path="Appoitments" element={<Appoitments />} />
-            <Route path="BecomeATeacher" element={<BecomeATeacher />} />
-              <Route path="/learning/*" element={<Learning />} />
-        </Routes>
- 
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="ProfilePage" element={<ProfilePage />} />
+        <Route path="About" element={<About />} />
+        <Route path="Contact" element={<Contact />} />
+        <Route path="SignIn" element={<SignIn />} />
+        <Route path="Appoitments" element={<Appoitments />} />
+        <Route path="BecomeATeacher" element={<BecomeATeacher />} />
+        <Route path="/learning/*" element={<Learning />} />
+        <Route path="/categories/*" element={<Categories />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Components/Categories.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Header from "./Header";
+import Footer from "./Footer";
+import "../styles/categories.css";
+import "../styles/responsive.css";
+import { Link } from "react-router-dom";
+
+const Categories = () => {
+  const [languages, setLanguages] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:8000/language/getAllLanguage"
+        );
+        console.log(response.data.data);
+        setLanguages(response.data.data);
+      } catch (error) {
+        console.error("Error fetching languages:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="categories">
+      <Header />
+      <div className="categories-container">
+        <p className="categories-text">Select Courses From Our Variety</p>
+        <div className="inside-catg-container">
+          {Object.keys(languages).map((categoryTitle) => (
+            <div key={categoryTitle} className="catg-div">
+              <p className="catg-title">{categoryTitle} Courses</p>
+
+              {languages[categoryTitle].map((language) => (
+                <div key={language.language_id} className="inside-catg-div">
+                  <div className="div1">
+                    <p>{language.language_name}</p>
+                  </div>
+                  <div className="div1">
+                    <Link to="/learning">
+                      <button className="enrol-btn">Enroll</button>
+                    </Link>
+                  </div>
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Categories;
