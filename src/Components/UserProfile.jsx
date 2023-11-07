3b0ade22bf0320506cae70a5dd1985ed001772b6
rasha <rashataher1245@gmail.com>
2023-11-07 14:19:49 +0200

    finished all the functionalities of the user's profile

--- src/Components/UserProfile.jsx
+++ src/Components/UserProfile.jsx
@@ -7,6 +7,7 @@ import { useState, useEffect } from "react";
 
 const UserProfile = () => {
   const [appointments, setAppointments] = useState([]);
+  const [registeredCourses, setRegisteredCourses] = useState([]);
 
   const [userData, setUserData] = useState({
     id: "",
@@ -14,14 +15,33 @@ const UserProfile = () => {
     email: "",
     password: "",
   });
+  const handleDropCourse = async (studentInfoId) => {
+    try {
+      const response = await axios.delete(`http://localhost:5000/studentInfo/dropCourse/${studentInfoId}`);
+      const data = response.data;
+
+      if (data.success) {
+        // Remove the deleted course from the state
+        setRegisteredCourses(prevCourses => prevCourses.filter(course => course.student_info_id !== studentInfoId));
+      } else {
+        console.error(data.message);
+      }
+    } catch (error) {
+      console.error('Error dropping course:', error);
+    }
+  };
+
   const handleSave = async () => {
     try {
-      const response = await axios.post("http://localhost:5000/student/updateStudent", {
-        id: userData.id,
-        name: userData.name,
-        email: userData.email,
-        password: userData.password
-      });
+      const response = await axios.post(
+        "http://localhost:5000/student/updateStudent",
+        {
+          id: userData.id,
+          name: userData.name,
+          email: userData.email,
+          password: userData.password,
+        }
+      );
 
       if (response.data.success) {
         alert("User information updated successfully");
@@ -34,21 +54,23 @@ const UserProfile = () => {
   };
   const cancelAppointment = async (id) => {
     try {
-      const response = await axios.post(`http://localhost:5000/appoitment/cancelAppointment/${id}`);
+      const response = await axios.post(
+        `http://localhost:5000/appoitment/cancelAppointment/${id}`
+      );
       if (response.data.success) {
         // If successful, update the status in the state
         setAppointments((prevAppointments) =>
           prevAppointments.map((appointment) =>
             appointment.appoitment_id === id
-              ? { ...appointment, status: 'Canceled' }
+              ? { ...appointment, status: "Canceled" }
               : appointment
           )
         );
       } else {
-        console.error('Error:', response.data.error);
+        console.error("Error:", response.data.error);
       }
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
     }
   };
 
@@ -62,7 +84,7 @@ const UserProfile = () => {
 
       const fetchData = async () => {
         try {
-          const response = await axios.get( 
+          const response = await axios.get(
             `http://localhost:5000/student/getStudentByEmail/${userEmail}`
           );
 
@@ -90,8 +112,23 @@ const UserProfile = () => {
           console.error("Error:", error);
         }
       };
-    
-    
+
+      const fetchStudentInformation = async () => {
+        try {
+          const response = await axios.get(
+            `http://localhost:5000/studentInfo/getStudentInformation/${userEmail}`
+          );
+          if (response.data.success) {
+            setRegisteredCourses(response.data.data);
+          } else {
+            console.error("Error:", response.data.error);
+          }
+        } catch (error) {
+          console.error("Error:", error);
+        }
+      };
+
+      fetchStudentInformation();
       fetchData();
       fetchAppoitment();
     }
@@ -136,91 +173,103 @@ const UserProfile = () => {
               )}
             </div>
           </div>
-          <div className="row">
-            <div>
-              <p className="user-info">Password: </p>
-              {userData && (
-                <input
-                  type="password"
-                  className="user-input"
-                  value={userData.password || ""}
-                  onChange={(e) =>
-                    setUserData({ ...userData, password: e.target.value })
-                  }
-                />
-              )}
-            </div>
 
-            
+          <div>
+            <p className="user-info">Password: </p>
+            {userData && (
+              <input
+                type="password"
+                className="user-input"
+                value={userData.password || ""}
+                onChange={(e) =>
+                  setUserData({ ...userData, password: e.target.value })
+                }
+              />
+            )}
           </div>
           <div className="button-container">
-        <button className="save-button" onClick={handleSave}> Save</button>
-      </div>
+            <button className="save-button" onClick={handleSave}>
+              {" "}
+              Save
+            </button>
+          </div>
           <div className="table-container">
             <p className="user-info"> Registered Courses</p>
-
             <table className="custom-table">
               <tr>
-                <th>Course Name</th>
-                <th>Teacher Name</th>
-                <th>Status</th>
-                <th>Score</th>
+                <th>Language Name</th>
+                {/* <th>Teacher Name</th> */}
+                <th>Days of attendance</th>
+                <th>Completed</th>
+                <th>Scores</th>
+                <th>Chapter Completed</th>
+                <th>Action</th>
               </tr>
-              <tr>
-                <td>Course 1</td>
-                <td>Teacher A</td>
-                <td>Completed</td>
-                <td>95</td>
-              </tr>
-              <tr>
-                <td>Course 2</td>
-                <td>Teacher B</td>
-                <td>In Progress</td>
-                <td>75</td>
-              </tr>
+              {registeredCourses.map((course) => (
+                <tr key={course.student_info_id}>
+                  <td>{course.language_id}</td>
+                  {/* <td>{course.teacher_name}</td> */}
+                  <td>{course.days_of_attendance}</td>
+                  <td>{course.completed ? "Completed" : "In Progress"}</td>
+                  <td>{course.scores_count}</td>
+                  <td>{course.chapter_completed}</td>
+                  <td>
+                    <button
+                      className="action-button delete"
+                      onClick={() => handleDropCourse(course.student_info_id)}
+                    >
+                      Drop Course
+                    </button>
+                  </td>
+                </tr>
+              ))}
             </table>
           </div>
           <div className="table-container">
-      <p className="user-info"> Your Appointments</p>
+            <p className="user-info"> Your Appointments</p>
 
-      <table className="custom-table">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Teacher Name</th>
-            <th>Date</th>
-            <th>Start Time</th>
-            <th>End Time</th>
-            <th>Status</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {appointments.map((appointment, index) => (
-            <tr key={index}>
-              <td>{appointment.appoitment_name}</td>
-              <td>{appointment.name}</td>
-              <td>{appointment.appoitment_date}</td>
-              <td>{appointment. appoitment_start_time}</td>
-              <td>{appointment.appoitment_end_time}</td>
-              <td>{appointment.status}</td>
-              <td>
-              <button
-                  className="action-button delete"
-                  onClick={() => cancelAppointment(appointment.appoitment_id)}
-                >  Cancel Appointment
-                </button>
-              </td>
-            </tr>
-        )  )}
-        </tbody>
-      </table>
-    </div>
+            <table className="custom-table">
+              <thead>
+                <tr>
+                  <th>Title</th>
+                  <th>Teacher Name</th>
+                  <th>Date</th>
+                  <th>Start Time</th>
+                  <th>End Time</th>
+                  <th>Status</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {appointments.map((appointment, index) => (
+                  <tr key={index}>
+                    <td>{appointment.appoitment_name}</td>
+                    <td>{appointment.name}</td>
+                    <td>{appointment.appoitment_date}</td>
+                    <td>{appointment.appoitment_start_time}</td>
+                    <td>{appointment.appoitment_end_time}</td>
+                    <td>{appointment.status}</td>
+                    <td>
+                      <button
+                        className="action-button delete"
+                        onClick={() =>
+                          cancelAppointment(appointment.appoitment_id)
+                        }
+                      >
+                        {" "}
+                        Cancel Appointment
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
       <Footer />
     </div>
   );
-}
+};
 
 export default UserProfile;
